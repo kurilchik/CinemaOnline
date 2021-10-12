27b281c7cb32b006d5fdc5435b1e808895e82ab2
andrew.kurilchik@gmail.com <andrew.kurilchik@gmail.com>
2021-10-12 21:49:55 +0300

    Added JavaScript to FilmView

--- CinemaOnline/CinemaOnline.WebApplication.PL/wwwroot/js/site.js
+++ CinemaOnline/CinemaOnline.WebApplication.PL/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    debugger;
+
+    $("select.cinema-selection").on('change', function () {
+        alert(this.value);
+        alert(window.nonID);
+    });
+});
